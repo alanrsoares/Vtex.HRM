50f44a6531feb72add26303e6da0dcaffd38de2b
Alan R. Soares <alanrsoars@gmail.com>
2013-03-27 17:45:52 -0300

    update gitignore & change partial files path

--- Vtex.HRM.Web/App/js/app.js
+++ Vtex.HRM.Web/App/js/app.js
@@ -3,7 +3,7 @@
 var app = angular.module('vtex-hrm', ['vtex-hrm.services', '$strap.directives']).
   config(function ($routeProvider) {
       $routeProvider.
-        when('/dashboard', { controller: DashboardController, templateUrl: 'hrm/app/partials/dashboard.html' }).
-        when('/about', { controller: AboutController, templateUrl: 'hrm/app/partials/about.html' }).
+        when('/dashboard', { controller: DashboardController, templateUrl: 'app/partials/dashboard.html' }).
+        when('/about', { controller: AboutController, templateUrl: 'app/partials/about.html' }).
         otherwise({ redirectTo: '/dashboard' });
   });
