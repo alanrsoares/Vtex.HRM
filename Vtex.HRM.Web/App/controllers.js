2b634dfb3e2e2baf36fd56f272ea4bc6c184a407
Alan R. Soares <alanrsoars@gmail.com>
2013-04-15 19:59:50 -0300

    Add temporizer to checks controller

--- Vtex.HRM.Web/App/controllers.js
+++ Vtex.HRM.Web/App/controllers.js
@@ -32,40 +32,99 @@ var ResourcesCtrl = function ($rootScope) {
 //#region Checks
 var ChecksCtrl = function ($scope, $rootScope, checks) {
 
+    var activeTab = null;
+
     var paneOptions = {};
 
-    $scope.paneOptions = function (filtered, tabName, alertClass) {
+    $scope.paneOptions = function (tab) {
         if (arguments.length === 0) {
             return paneOptions;
         } else {
             paneOptions = {
-                filtered: filtered,
-                alertClass: alertClass,
-                tabName: tabName
+                filtered: tab.checks,
+                tabName: tab.label,
+                alertStyle: tab.alertStyle,
             };
+            activeTab = tab;
         }
     };
 
-    $scope.filtered = [];
+    $scope.fetchAllChecks = function () {
+        // get all checks
+        checks.get(function (data) {
 
-    // get all checks
-    checks.get(function (data) {
-        $scope.checks = data.checks;
-        $scope.up = _.where($scope.checks, { status: 'up' });
-        $scope.down = _.where($scope.checks, { status: 'down' });
-        $scope.beta = _.filter($scope.checks, function (check) {
-            var pattern = /- Beta/;
-            return pattern.test(check.name);
+            $scope.checks = data.checks;
+            $scope.up = _.where($scope.checks, { status: 'up' });
+            $scope.down = _.where($scope.checks, { status: 'down' });
+            $scope.beta = _.filter($scope.checks, function (check) {
+                var pattern = /- Beta/;
+                return pattern.test(check.name);
+            });
+            $scope.stable = _.filter($scope.checks, function (check) {
+                var pattern = /- Stable/;
+                return pattern.test(check.name);
+            });
+
+            //#region Tabs
+            $scope.tabs = [
+                {
+                    label: "All",
+                    badgeStyle: "",
+                    alertStyle: "alert-info",
+                    checks: $scope.checks,
+                    active: ($scope.down.length === 0)
+                },
+                {
+                    label: "Up",
+                    badgeStyle: "badge-success",
+                    alertStyle: "alert-success",
+                    checks: $scope.up,
+                    active: false
+                },
+                {
+                    label: "Down",
+                    badgeStyle: "badge-important",
+                    alertStyle: "alert-error",
+                    checks: $scope.down,
+                    active: ($scope.down.length > 0)
+                },
+                {
+                    label: "Stable",
+                    badgeStyle: "badge-info",
+                    alertStyle: "alert-info",
+                    checks: $scope.stable,
+                    active: false
+                },
+                {
+                    label: "Beta",
+                    badgeStyle: "badge-warning",
+                    alertStyle: "",
+                    checks: $scope.beta,
+                    active: false
+                }
+            ];
+            
+            //#endregion
+
+            // sets paneOptions initial state
+            activeTab = _.where($scope.tabs, { active: true })[0];
+            $scope.paneOptions(activeTab);
+
         });
-        $scope.stable = _.filter($scope.checks, function (check) {
-            var pattern = /- Stable/;
-            return pattern.test(check.name);
-        });
+    };
 
-        // sets paneOptions initial state
-        $scope.paneOptions($scope.checks, 'ALL', 'info');
+    var init = function () {
 
-    });
+        $scope.fetchAllChecks();
+
+        window.setInterval(function () {
+
+            $scope.fetchAllChecks();
+
+        }, 60 * 1000);
+    };
+
+    init();
 };
 
 var ChecksDetailCtrl = function ($scope, $rootScope, $routeParams, checks) {
